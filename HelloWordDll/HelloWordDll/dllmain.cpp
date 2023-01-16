994af3b5fad963add2c68a7705ceb4211a6957ac
xodmf1215 <xodmf1215@users.noreply.github.com>
2023-01-16 17:08:36 +0900

    add dll function
    1. __declspec(dllexport/dllimport) 로 dll 함수임을 명시
    2. 함수 정의 완료

--- HelloWordDll/HelloWordDll/dllmain.cpp
+++ HelloWordDll/HelloWordDll/dllmain.cpp
@@ -1,5 +1,9 @@
-﻿// dllmain.cpp : DLL 애플리케이션의 진입점을 정의합니다.
+﻿// dllmain.cpp : DLL 애플리케이션의 진입점을 정의합니다. 프로젝트 설정에서 '미리 컴파일된 헤더' 옵션을 쓰고있다.
+// visual studio 2017 버전 전에는 stdafx.h 를 주로 썼다.
 #include "pch.h"
+
+#define EXPORTING_DLL
+#include "helloDLL.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -17,3 +21,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+char* HelloWorld()
+{
+    char* res;
+    res = (char*)malloc(sizeof(char) * 11);
+    strcpy(res, "HelloWorld");
+}
